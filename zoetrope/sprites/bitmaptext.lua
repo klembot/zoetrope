116abf51f627bd460416b74950ff97a8cb7c7216
Chris Klimas <chris@twofoldsecret.com>
2012-02-06 23:22:12 -0500

    Add BitmapText functionality
    This is squashed from the bitmaptext branch.

--- zoetrope/sprites/bitmaptext.lua
+++ zoetrope/sprites/bitmaptext.lua
@@ -12,25 +12,42 @@ BitmapText = Sprite:extend({
 
 	-- Property: text
 	-- What text to display.
+	text = '',
+
+	-- Propert: align
+	-- Can be either 'left', 'center' or 'right'.
+	align = 'left',
 
 	-- Property: leading
 	-- Space between lines, in pixels.
-	leading = 4,
+	leading = 0,
 
 	-- Property: tracking
 	-- Space between characters, in pixels.
-	tracking = 2,
+	tracking = 0,
 
 	-- Property: wordWrap
 	-- Word wrap the text to fit the sprite boundaries?
 	wordWrap = true,
 
+	-- private property: set
+	-- tracks whether we need to re-layout our lines
+	-- because one of our display properties has changed
+	set = {},
+
 	layoutLines = function (self)
-		local textLines = split(this.text, '\n')
+		local textLines = split(self.text, '\n')
 		local lineLength = #textLines
 
-		for i = 1, lineLength do
-			local width = this.font:textWidth(textLines[i], this.tracking)
+		self.lines = {}
+
+		-- this is a while loop because lineLength will
+		-- probably change inside the loop
+
+		local i = 1
+
+		while i <= lineLength do
+			local width = self.font:textWidth(textLines[i], self.tracking)
 
 			-- if word wrap is enabled, keep pushing words off the
 			-- end of the line until the width is less than sprite width
@@ -38,18 +55,43 @@ BitmapText = Sprite:extend({
 			if self.wordWrap and width > self.width then
 				lineLength = lineLength + 1
 
-				-- if we are on the final line, add a new one
-
-				if i + 1 > lineLength then
-					table.insert(textLines, '')
-				end
-
 				-- if the next line is blank (e.g. just a newline)
 				-- preserve the space by adding a line above it
 
-				if (
+				if textLines[i + 1] == '' then
+					table.insert(textLines, i + 1, '')
+				end
 
+				-- if we are on the final line, add a new one
+
+				if i + 1 > lineLength - 1 then
+					table.insert(textLines, '')
+				end
+
+				-- start shifting words until we're below the width 
+
+				while width > self.width do
+					spaceIndex = string.find(textLines[i], '%s%S+$')
+					if not spaceIndex then break end
+
+					textLines[i + 1] = string.sub(textLines[i], spaceIndex) .. textLines[i + 1] 
+					textLines[i] = string.sub(textLines[i], 1, spaceIndex - 1)
+					width = self.font:textWidth(textLines[i], self.tracking)
+				end
+
+				-- we always end up with a leading space on the following line
+				textLines[i + 1] = string.sub(textLines[i + 1], 2)
 			end
+
+			self.lines[i] = { text = textLines[i], inset = 0 }
+
+			if self.align == 'center' then
+				self.lines[i].inset = math.floor((self.width - width) / 2)
+			elseif self.align == 'right' then
+				self.lines[i].inset = self.width - width
+			end
+
+			i = i + 1
 		end
 	end,
 
@@ -65,7 +107,7 @@ BitmapText = Sprite:extend({
 		end
 
 		for _, line in pairs(self.lines) do
-			self.font:drawText(line.text, line.inset, y, self.tracking)
+			self.font:drawText(line.text, self.x + line.inset, y, self.tracking)
 			y = y + self.font.height + self.leading
 		end
 	end
