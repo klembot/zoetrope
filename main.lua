432072ea4e243ad412bc8e0cd4f2be95e1d9937e
Chris Klimas <chris@twofoldsecret.com>
2012-02-10 18:03:46 -0500

    Move tests into single require statement

--- main.lua
+++ main.lua
@@ -1,17 +1,4 @@
-require 'tests.helloworld'
-require 'tests.benchmark'
-require 'tests.collisions'
-require 'tests.emitters'
-require 'tests.focus'
-require 'tests.input'
-require 'tests.maps'
-require 'tests.scrolling'
-require 'tests.sounds'
-require 'tests.spritetypes'
-require 'tests.timers'
-require 'tests.tweens'
-require 'tests.ui'
-
+require 'tests'
 require 'zoetrope.utils.debugwatch'
 
 function love.load()
