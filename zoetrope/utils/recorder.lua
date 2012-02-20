373cbdca8bd7814442b2039a55f43025bc4ac99d
Chris Klimas <chris@twofoldsecret.com>
2012-02-19 22:31:56 -0500

    Rework Recorder to avoid Love-isms, add docs
    Now playback talks directly to the key handlers instead of stuffing
    events into the queue.

--- zoetrope/utils/recorder.lua
+++ zoetrope/utils/recorder.lua
@@ -1,7 +1,7 @@
 -- Class: Recorder
 -- This records the user's inputs for playback at a later time,
 -- or saving to a file. You must start recording *after* all input
--- devices are set up, and you should only run one recorder at a time.
+-- objects are set up, and you should only run one recorder at a time.
 --
 -- Extends:
 --		<Sprite>
@@ -9,11 +9,27 @@
 require 'zoetrope.core.sprite'
 
 Recorder = Sprite:extend({
+	-- private property: elapsed
+	-- either time elapsed while recording or playing back, in seconds
 	elapsed = 0,
 
+	-- Constant: IDLE
+	-- The recorder is currently doing nothing.
 	IDLE = 'idle',
+
+	-- Constant: RECORDING
+	-- The recorder is currently recording user input.
 	RECORDING = 'recording',
+
+	-- Constant: PLAYING
+	-- The recorder is currently playing back user input.
 	PLAYING = 'playing',
+
+	-- Property: state
+	-- One of the state constants, indicates what the recorder is currently doing.
+
+	-- Property: record
+	-- A table of inputs with timing information.
 
 	new = function (self, obj)
 		obj = self:extend(obj)
@@ -23,30 +39,59 @@ Recorder = Sprite:extend({
 		return obj
 	end,
 
+	-- Method: startRecording
+	-- Begins recording user inputs. If the recorder is already recording,
+	-- this has no effect.
+	--
+	-- Arguments:
+	--		record - Record to use. Any existing data is appended to.
+	--				 If omitted, the current record is used. If the current
+	--				 record is unset, this creates a new record.
+	--
+	-- Returns:
+	--		nothing
+
 	startRecording = function (self, record)
 		if self.state == Recorder.RECORDING then return end
 
 		-- set up properties
-		self.record = record or {}
+		self.record = record or self.record or {}
 		self.state = Recorder.RECORDING
 		self.elapsed = 0
 
 		-- insert ourselves into event handlers
-		local this = self
+		self:stealInputs()
 
-		self.origKeyPressed = love.keypressed
-		love.keypressed = function (key, code) this:keyPressed(key, code) end
-		self.origKeyReleased = love.keyreleased
-		love.keyreleased = function (key, code) this:keyReleased(key, code) end
 	end,
+
+	-- Method: stopRecording
+	-- Stops recording user inputs. If the recorder wasn't recording anyway,
+	-- this does nothing.
+	-- 
+	-- Arguments:
+	--		none
+	-- 
+	-- Returns:
+	--		nothing
 
 	stopRecording = function (self)
 		if self.state ~= Recorder.RECORDING then return end
 
 		self.state = Recorder.IDLE
+		self:restoreInputs()
 		love.keypressed = self.origKeyPressed
 		love.keyreleased = self.origKeyReleased
 	end,
+
+	-- Method: startPlaying
+	-- Starts playing back user inputs.
+	--
+	-- Arguments:
+	--		record - Record to play back. If ommitted, this uses
+	--				 the recorder's record property.
+	--
+	-- Returns:
+	--		nothing
 
 	startPlaying = function (self, record)
 		record = record or self.record
@@ -54,22 +99,47 @@ Recorder = Sprite:extend({
 
 		self.elapsed = 0
 		self.playbackIndex = 1
+		self:stealInputs()
 	end,
+
+	-- Method: stopPlaying
+	-- Stops playing back user inputs.
+	--
+	-- Arguments:
+	--		none
+	--
+	-- Returns:
+	--		nothing
 
 	stopPlaying = function (self)
 		self.state = Recorder.IDLE
+		self:restoreInputs()
 	end,
 
-	keyPressed = function (self, key, unicode)
-		table.insert(self.record, { self.elapsed, 'kp', key, unicode })
+	stealInputs = function (self)
+		local this = self
+
+		self.origKeyPressed = love.keypressed
+		love.keypressed = function (key, code) this:recordKeyPress(key, code) end
+		self.origKeyReleased = love.keyreleased
+		love.keyreleased = function (key, code) this:recordKeyRelease(key, code) end
+	end,
+
+	restoreInputs = function (self)
+		love.keypressed = self.origKeyPressed
+		love.keyreleased = self.origKeyReleased
+	end,
+	
+	recordKeyPress = function (self, key, unicode)
+		table.insert(self.record, { self.elapsed, 'keypress', key, unicode })
 
 		if self.origKeyPressed then
 			self.origKeyPressed(key, unicode)
 		end
 	end,
 
-	keyReleased = function (self, key, unicode)
-		table.insert(self.record, { self.elapsed, 'kr', key, unicode })
+	recordKeyRelease = function (self, key, unicode)
+		table.insert(self.record, { self.elapsed, 'keyrelease', key, unicode })
 
 		if self.origKeyReleased then
 			self.origKeyReleased(key, unicode)
@@ -83,8 +153,13 @@ Recorder = Sprite:extend({
 
 		if self.state == Recorder.PLAYING and self.elapsed > self.record[self.playbackIndex][1] then
 			local event = self.record[self.playbackIndex]
-			love.event.push(event[2], event[3], event[4], event[5])
 			
+			if event[2] == 'keypress' and self.origKeyPressed then
+				self.origKeyPressed(event[3], event[4])
+			elseif event[2] == 'keyrelease' and self.origKeyReleased then
+				self.origKeyReleased(event[3], event[4])
+			end
+
 			self.playbackIndex = self.playbackIndex + 1
 
 			if self.playbackIndex > #self.record then
