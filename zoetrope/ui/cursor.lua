f75041513a9fea456f791dacaaf319a2435ccbd8
Chris Klimas <chris@twofoldsecret.com>
2012-06-23 20:12:28 -0400

    Document Cursor.hotspot

--- zoetrope/ui/cursor.lua
+++ zoetrope/ui/cursor.lua
@@ -2,6 +2,11 @@
 -- A cursor is a group that follows the user's cursor.
 
 Cursor = Group:extend{
+	-- Property: hotspot
+	-- A table with x and y offsets for the cursor. e.g. if
+	-- you want the user to click with a center of a crosshairs
+	-- image, set this to half the width and half the height of
+	-- the image.
 	hotspot = { x = 0, y = 0 },
 
 	new = function (self, obj)
